66698fe8f5b5cef486e26f7a80320394f1ba49c9
vinh-ngo-webjet <vinh.ngo@webjet.com.au>
2020-08-25 09:20:05 +1000

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,9 @@ tag=$TAG
 registry=$CONTAINERREGISTRY
 [ -z "$registry" ] && registry=$5
 repository=$6
+echo "---"
 ls
+echo "---"
 # response=$(curl -s -X POST "http://kubebot.default/deploy/$1/$2/$3/$4?registry=$5&repository=$6" \
 #         --data-binary "@deployment.yaml" \
 #         -H 'Content-Type: application/yaml' \
