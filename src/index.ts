a75d98af425e1f7138f879fe311e7f849fce0ccd
Kevin-Li-Webjet <kevin.li@webjet.com.au>
2022-12-13 16:49:40 +1100

    replace ... in header

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ try {
     const githubRepository = core.getInput('repositoryfullname');
     const githubOwner = core.getInput('owner');
     const gitURL = "https://github.com/" + core.getInput('repositoryfullname');
-    const headCommitMsg = String(core.getInput('headcommit')).replace(/\n/g, ' ').replace(/\r/g, ' ');
+    const headCommitMsg = String(core.getInput('headcommit')).replace(/\n/g, ' ').replace(/\r/g, ' ').replace(/…/g, '');
     console.dir(headCommitMsg);
     if (!kubebot) {
       throw new Error('kubebot url is needed!');
