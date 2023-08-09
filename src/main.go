acb436ab892a99e71b77b031cff5f94c4420de2a
marceldempers <marcel.dempers@webjet.com.au>
2023-08-09 10:28:01 +1000

    add trace id and improve messaging

--- src/main.go
+++ src/main.go
@@ -8,6 +8,8 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"github.com/rs/xid"
 	"time"
 	"fmt"
 	"errors"
@@ -34,17 +36,18 @@ func init() {
 
 func main() {
 
-	log.Info("validating input...")
+	fmt.Println("validating input...")
 	getValidateInput()
 
-	log.Info("reading deployment manifest...")
+	fmt.Println("reading deployment manifest...")
 	yamlBytes, err := ioutil.ReadFile(manifest)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Info("starting deployment...")
-	deploy(kubebotUrl, yamlBytes)
+	fmt.Println("starting deployment...")
+	traceID := xid.New().String()
+	deploy(kubebotUrl, yamlBytes, traceID)
 }
 
 func getInput(name string) string {
@@ -63,9 +66,6 @@ func getValidateInput() {
 		panic("manifest input is required!")
 	} else {
 		if _, err := os.Stat(manifest); errors.Is(err, os.ErrNotExist) {
-			log.WithFields(log.Fields{
-				"error": err,
-			}).Error("manifest file not found!")
 			panic("manifest: " + manifest + " not found!")
 		}
   }
@@ -120,7 +120,7 @@ func getValidateInput() {
 
 }
 
-func deploy(kubebotUrl string, yaml []byte) {
+func deploy(kubebotUrl string, yaml []byte, traceID string) {
 	
 	requestBody := bytes.NewBuffer(yaml)
 	requestUrl := fmt.Sprintf("%s/deploy/%s/%s/%s/%s?registry=%s&repository=%s" ,kubebotUrl, environment, namespace, serviceName, tag, registry, repo)
@@ -133,6 +133,7 @@ func deploy(kubebotUrl string, yaml []byte) {
 		panic("error creating deployment request! see error above for more details")
 	}
 
+	req.Header.Set("x-trace-id", traceID)
 	req.Header.Set("Content-Type", "application/yaml")
 	req.Header.Set("GITHUB-REPO-URL", gitURL)
 	req.Header.Set("GITHUB-REPO", githubRepository)
@@ -163,7 +164,14 @@ func deploy(kubebotUrl string, yaml []byte) {
 	res.Body.Close()
   
 	if res.StatusCode != 200 {
-		panic("Please check your deployment logs in SumoLogic with query `_sourcecategory = k8s/dev/bots/kubebot` , and verify the cluster with KUBECTL commands to troubleshoot further")
+
+		sumoQuery := "_sourcecategory = k8s/dev/bots/kubebot AND " + traceID
+		sumoUrl := "https://webjet.au.sumologic.com/ui/#/search/create?query=" + url.QueryEscape(sumoQuery)
+		
+		fmt.Println("DEPLOYMENT FAILED, please follow 2 steps below to troubleshoot")
+		fmt.Println("1 - Check your deployment with KUBECTL commands")
+		fmt.Println("2 - Check your deployment logs in Sumologic: " + sumoUrl)
+		panic("deployment failed!")
 	}
 	
 }
